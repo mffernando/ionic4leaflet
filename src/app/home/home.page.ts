8f2c62e6c285ccc5f450066f3c301daa8587f992
Fernando <fmti91@hotmail.com>
2019-08-14 20:14:48 -0300

    First commit - Ionic 4 + Leaflet and geolocation Project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import leaflet from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('map', null) mapContainer: ElementRef;
+  map: any;
+  constructor(public navCtrl: NavController) {
 
-  constructor() {}
+  }
 
+  ionViewDidEnter() {
+    this.loadmap();
+  }
+
+  loadmap() {
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(this.map);
+    this.map.locate({
+      setView: true,
+      maxZoom: 10
+    }).on('locationfound', (e) => {
+      let markerGroup = leaflet.featureGroup();
+      let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
+        alert('Marker clicked');
+      })
+      markerGroup.addLayer(marker);
+      this.map.addLayer(markerGroup);
+    }).on('locationerror', (err) => {
+      alert(err.message);
+    })
+  }
 }
